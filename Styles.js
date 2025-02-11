4a0d910be7bab95aeabd4cba48e5d78abd6f1b31
Authcornelius <corneliusedos@gmail.com>
2025-02-11 23:43:55 +0100

    updated

--- Styles.js
+++ Styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: 50,
+    paddingVertical: 30,
     paddingHorizontal: 20,
   },
 
